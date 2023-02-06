e163a027947cbe71ab284cc586004c88dace1103
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-02-06 14:39:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'https://dulink.in')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'dulink.in')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'cbee056833ddeee922eeebdcd85c5997fcecb230')
 
      # Auto Delete For Group Message (Self Delete) #
